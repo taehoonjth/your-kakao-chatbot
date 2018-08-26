322d7e431a07bca7d050817f34abf71db164c881
Mark <jungtaehoon@icloud.com>
2018-08-26 18:06:16 +0900

    delete solution

--- server.js
+++ server.js
@@ -6,48 +6,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/keyboard', function (req, res) {
-  var response = {
-    "type": "text"
-  };
-  res.header("Content-Type",'application/json; charset=utf-8');
-  res.send(JSON.stringify(response));
+
 });
 
 app.post('/message', function (req, res) {
-  var response;
-  var userMessage = req.body.content;
-  if(userMessage === '하이') {
-    response = {
-      "message":{
-        "text": "반가워, 나한테 시킬 일 있니?"
-      }
-    };
-  } else if(userMessage.indexOf('몇일') !== -1) {
-    response = {
-      "message":{
-        "text": `오늘은 ${new Date().getDate()}일 이야.`
-      }
-    };
-  } else if(userMessage.indexOf('원빈') !== -1) {
-    response = {
-      "message":{
-        "text": "자 여기 원빈 사진이야.",
-        "photo": {
-          "url": "https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/1hYk/image/AqQJUQkNYKq2MEC18kF2ml8wLBA",
-          "width": 467,
-          "height": 552
-        }
-      }
-    };
-  } else {
-    response = {
-      "message":{
-        "text": "무슨 말인지 모르겠어. 미안."
-      }
-    };
-  }
-  res.header("Content-Type",'application/json; charset=utf-8');
-  res.send(JSON.stringify(response));
+
 });
 
 app.post('/friend', (req, res) => {
